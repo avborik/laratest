5d6b2cfa6bbbc50cca5c52835ed914b303b3311e
AvBorikDeveloper <avboris28@gmail.com>
2019-03-28 01:04:08 +1300

    db practice

--- routes/web.php
+++ routes/web.php
@@ -64,6 +64,27 @@ Route::get('/posts', 'PostsController@index')->name('posts');
 Route::get('/posts/{id}',function($id){
     //return 'Hey Pandas';
     //$posts = Post::all();
-    $post = Post::find($id);
-    return $post;
+    // $post = Post::find($id);
+    // $post = Post::orderBy('id', 'asc')->get();
+    // $post = Post::where('is_admin', 1)
+    // ->where('votes', 5)
+    // ->get();
+
+    // $post = Post::all()->first();
+    // $post = new Post;
+    // $post->title = 'Italy is awesome';
+    // $post->body = '"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."';
+    // $post->votes = 5;
+    // $post->is_admin = 1;
+
+    // $post->save();
+    // $posts = Post::find(1);
+
+    // $posts->title = 'Awesome Titans';
+    // $posts->save();
+   // return $post;
+
+   $posts = Post::where('is_admin', 1)->update([
+       'votes' => 100
+   ]);
 });
