e7439fb176f1f344514683b4c2b9f99b343648b2
AvBorikDeveloper <avboris28@gmail.com>
2019-04-05 00:36:47 +1300

    pagination

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="card-header">
+        All posts
+    </div>
+    <div class="card-body">
+        <ul>
+            @foreach ($posts as $post)
+                <li>
+                <a href="#">{{$post->title}}</a>
+                </li>
+            @endforeach
+        </ul>
+        {{$posts->appends(['myArg' => 'true'])->links()}}
+    </div>
+</div>
+@endsection
